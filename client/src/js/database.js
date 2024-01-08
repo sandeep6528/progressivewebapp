db27e166a1383857600cb042c72c1d3d16cdd764
Sandeep Meegahapola <smeegahapola5@gmail.com>
2024-01-08 11:30:43 -0600

    emplemented database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('update database')
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log(result.value)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('readfromdatabase')
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  if(result){
+    console.log('datainthedatabase',result.value)
+  }else{
+    console.log('nodatainthedatabase')
+  }
+  return result.value
+}
 
 initdb();
